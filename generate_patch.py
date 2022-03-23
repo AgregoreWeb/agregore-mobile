3fa69bf27eaf7f75216ff4e0d9bcb5a13ad656b7
mauve <ranger@mauve.moe>
2022-03-23 13:07:04 -0400

    Add patch for rebrainding to Agregore, add script for genrating patches

---
+++ generate_patch.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+'''
+This script will generate a new patch from the latest commit.
+'''
+
+import os
+import argparse
+import subprocess
+
+from _load_vars import load_vars
+
+parser = argparse.ArgumentParser(
+    description='Generate patch from the latest commit'
+)
+
+parser.add_argument(
+    '--n',
+    default="HEAD^",
+    help="Number of commits to use for the patch"
+)
+
+args = load_vars(parser)
+
+chromium = args["chromium"]
+env = args["env"]
+root = args["root"]
+n = args["n"]
+
+patch_dir = os.path.join(root, 'patches')
+
+print("Generating patch from latest commit")
+to_run = f'git format-patch -n {n} -o {patch_dir}'
+subprocess.run(to_run, cwd=chromium, shell=True, check=True, env=env)
+
+print("Done!")
